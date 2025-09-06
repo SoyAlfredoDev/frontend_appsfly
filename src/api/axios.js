7579f20d9e5bcb3b47150c94b35cd0b78290b177
soyAlfredoDev <soyalfredo.dev@gmail.com>
2025-09-06 16:46:52 +0100

    update

---
+++ src/api/axios.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+
+
+/*const instance = axios.create({
+    baseURL: 'http://localhost:3000/api',
+    withCredentials: true
+
+});*/
+
+
+
+const instance = axios.create({
+    baseURL: 'https://backend-appsfly.onrender.com/api',
+    withCredentials: true
+
+});
+
+
+export default instance;
