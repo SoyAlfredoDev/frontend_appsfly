814f2609f56907298d4183c8e8accf1178d44e33
soyAlfredoDev <soyalfredo.dev@gmail.com>
2025-10-18 02:54:23 +0100

    se cambia la base de la url para la api

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-
+/*
 
 const instance = axios.create({
     baseURL: 'http://localhost:3000/api',
@@ -9,12 +9,18 @@ const instance = axios.create({
 })
 
 
-/*
+
 const instance = axios.create({
     baseURL: 'https://backend-appsfly.onrender.com/api',
     withCredentials: true
 
 });
 */
+
+const instance = axios.create({
+    baseURL: 'http://64.176.14.37:3000/api',
+    withCredentials: true
+
+})
 
 export default instance;
