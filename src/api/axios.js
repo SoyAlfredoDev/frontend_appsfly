95e2bacf9613894aa5e10491699881201ee690d7
soyAlfredoDev <soyalfredo.dev@gmail.com>
2025-10-22 00:58:24 +0100

    up

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,30 +1,22 @@
 import axios from "axios";
 
-
 /*const instance = axios.create({
     baseURL: 'http://localhost:3000/api',
     withCredentials: true
 
-})*/
-
-
-
-
-
+})
 
 const instance = axios.create({
     baseURL: 'https://backend-appsfly.onrender.com/api',
     withCredentials: true
 
 });
-/*
+*/
 
 const instance = axios.create({
     baseURL: 'https://api.appsfly.app/api',
     withCredentials: true
 
-})
-
-*/
+});
 
 export default instance;
